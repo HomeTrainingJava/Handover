b29977e74ef95e1b0c9b8eee682acc1ad866e8a2
Veronika <VERANIKAVALOSHCHYK1@gmail.com>
2015-07-04 18:02:24 +0300

    I have transfered (strBuilder.append("\n")) in another place.

--- HelloWorld/src/org/local/test/HW.java
+++ HelloWorld/src/org/local/test/HW.java
@@ -16,22 +16,18 @@ public class HW {
 			String[] letterR = getLetterR().split("\n");
 			String[] letterD = getLetterD().split("\n");
 			String[][] wholeWord = {letterH, letterE,letterL,letterL,letterO,letterW,letterO,letterR,letterL,letterD};
-			String joinedLetter = "";						
+			String joinedLetter = "";					
 			for (int i=0;i<letterArrayLength; i++){				
 				for (int j=0;j<wholeWord.length; j++){
 					String[] currentLetterArray = wholeWord[j];
-					
 					if (currentLetterArray.length > i) 
 					{
-						
 						strBuilder.append(currentLetterArray[i]);
 						strBuilder.append(" ");
-						
+						strBuilder.append("\n");
 					}
 				}				
-			}			
-			String strArray= new String ();
-			strBuilder.append("\n");
+			}	
 			String phrase = strBuilder.toString();
 			//Ask user for its own symbol
 			System.out.println("¬ведите данные");
@@ -50,7 +46,7 @@ public class HW {
 			}
 		 }
 	}
-	
+		
 	private static String swapSymbols(String word, char symbol){
 		
 		//преобразование char to string
@@ -66,40 +62,40 @@ public class HW {
 	
 		protected static String getLetterO() {
 		// TODO Auto-generated method stubФ
-		String letterO=   " * \n* *\n* *\n* *\n * ";
+		String letterO=   "****\n*  *\n*  *\n*  *\n*  *\n****";
 		return letterO;
 	}	
 
 	private static String getLetterL() {
 		// TODO Auto-generated method stub
-		String letterL=   "*  \n*  \n*  \n*  \n***";
+		String letterL=   "*   \n*   \n*   \n*   \n*   \n****";
 		return letterL;
 				
 	}
 
 	private static String getLetterE() {
 		// TODO Auto-generated method stub
-		String letterE=   "***\n*  \n***\n*  \n***";
+		String letterE=   "****\n*   \n****\n*   \n*   \n****";
 		return letterE;
 	}
 
 	private static String getLetterH() {
 		// TODO Auto-generated method stub
-		 String letterH =   "* *\n* *\n***\n* *\n* *";
-		 return letterH;
+		 String letterH =   "*   *\n*   *\n****\n*  *\n*  *\n****";
+		 return letterH; 
 	}
 	
 	private static String getLetterR(){
-		String letterR=   "****\n* *\n***\n** \n* *";		
+		String letterR=   "****\n*  *\n* * \n*  *\n*   *";		
 		return letterR;
 	}
 
 	private static String getLetterW(){
-		String letterW=   "* *\n***\n* *";		
+		String letterW=   "*           *\n *         * \n  *   *   *  \n   * * * *   \n    *   *    ";		
 		return letterW;
 	}	
 	private static String getLetterD(){
-		String letterD=   "***\n****\n***";			
+		String letterD=   "*** \n*  *\n*  *\n*  *\n*** ";			
 		return letterD;
 	}
 
